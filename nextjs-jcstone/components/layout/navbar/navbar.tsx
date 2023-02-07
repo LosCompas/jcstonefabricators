97ee99b2d5942c806154fe758ff2f7262d057cfc
Fernando Caudillo <caudillojavier@gmail.com>
2023-02-06 19:27:07 -0800

    Fix Navbar and package.json to deploy to firebase

--- nextjs-jcstone/components/layout/navbar/navbar.tsx
+++ nextjs-jcstone/components/layout/navbar/navbar.tsx
@@ -91,7 +91,7 @@ export default function Navbar() {
           <Box pb={4} display={{ md: 'none' }}>
             <Stack as={'nav'} spacing={4}>
               {Links.map((link) => (
-                <NavLink key={link}>{link}</NavLink>
+                <NavLink key={link.path} path={link.path}>{link.name}</NavLink>
               ))}
             </Stack>
           </Box>
